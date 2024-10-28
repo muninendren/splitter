93ad63769098f7c22eeba40c7c037357ec9a77fd
rmuninendren <rmuninendren@invidi.com>
2024-10-28 22:52:07 +0530

    intial setup

---
+++ src/main/java/com/example/Spliter/Expenses/Software/SpliterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spliter.Expenses.Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpliterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpliterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spliter/Expenses/Software/SpliterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spliter.Expenses.Software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
